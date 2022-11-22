29f4d52168483737d9ee0b75866d0340dd6576a4
mutalovtemur <115086937+mutalovtemur@users.noreply.github.com>
2022-11-22 21:24:10 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+window.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('scroll', function (){
+        const header = this.document.querySelector('header');
+        header.classList.toggle('sticky', window.scrollY > 0)
+    })
+
+    const menuBtn = document.querySelector('.menu-btn');
+    const navigation = document.querySelector('.navigation');
+    const navigationItems = document.querySelectorAll('.navigation a')
+
+    menuBtn.addEventListener('click', () => {
+        menuBtn.classList.toggle('active')
+        navigation.classList.toggle('active')
+    })
+
+    navigationItems.forEach(navItem => {
+        navItem.addEventListener('click', () => {
+            menuBtn.classList.remove('active')
+            navigation.classList.remove('active')
+        })
+    })
+
+    const scrollBtn = document.querySelector('.scrollToTop-btn')
+    window.addEventListener('scroll', () => {
+        scrollBtn.classList.toggle('active',scrollY > 500)
+    })
+    scrollBtn.addEventListener('click', () => {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    } )
+
+
+
+    window.addEventListener('scroll', () => {
+        let windowHeight = window.innerHeight;
+        let revealTop = reveals[i].getBoundingClientRect().top;
+        let revealPoint = 50;
+
+        if(revealTop < windowHeight - revealPoint) {
+            reveal[i].classList.add('active')
+        }
+    } )
+})
